a757517502ef298c8022fb52561b3d6bdd464e9a
welhox <casimir.lundberg@gmail.com>
2024-05-15 15:03:24 +0300

    removed old textures freom header

--- include/cub3d.h
+++ include/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rboudwin <rboudwin@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: clundber <clundber@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 13:58:21 by clundber          #+#    #+#             */
-/*   Updated: 2024/05/15 14:23:40 by rboudwin         ###   ########.fr       */
+/*   Updated: 2024/05/15 15:02:00 by clundber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,10 +46,6 @@ typedef struct s_map
 	int			p_pos_x;
 	int			p_pos_y;
 	double		p_orient;
-	char		*no_text;
-	char		*so_text;
-	char		*we_text;
-	char		*ea_text;
 	int			*ceiling;
 	int			*floor;
 	t_images	*images; 
