90d80013c44b2f564f7c04de7ea9892cdfff4fd5
Casimir Lundberg <clundber@student.hive.fi>
2024-05-20 17:19:37 +0300

    both x and y axis working now

--- srcs/ray_casting.c
+++ srcs/ray_casting.c
@@ -6,7 +6,7 @@
 /*   By: clundber <clundber@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 11:27:52 by clundber          #+#    #+#             */
-/*   Updated: 2024/05/20 16:14:51 by clundber         ###   ########.fr       */
+/*   Updated: 2024/05/20 17:11:05 by clundber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,58 +18,66 @@ void	get_len(t_map *map, int *x_len, int *y_len, int deg)
 
 	corner = 0;
 		corner = (map->p_orient + (deg * DEG_2_RAD));
-	if (corner / DEG_2_RAD > 90 && corner / DEG_2_RAD < 270)
-		(*y_len) = (64 - ((map->p_pos_y +32) % 64)) / cos((corner));
+ 	if (corner / DEG_2_RAD > 90 && corner / DEG_2_RAD < 270)
+		(*y_len) = (64 - ((map->p_pos_y +32) % 64)) / cos(corner);
 	else
-		(*y_len) = ((map->p_pos_y +32) % 64) / cos((corner));
-    if (corner / DEG_2_RAD > 0 && corner / DEG_2_RAD < 180)
-		(*x_len) = (64 - ((map->p_pos_x +32) % 64)) / cos((corner));
+		(*y_len) = ((map->p_pos_y +32) % 64) / cos(corner);
+	if (corner / DEG_2_RAD > 90)
+		corner += (-90 * DEG_2_RAD);
 	else
-		(*x_len) = ((map->p_pos_x +32) % 64) / cos((corner));	
-	if (*y_len < 0)
+		corner += (270 * DEG_2_RAD);
+     if (corner / DEG_2_RAD > 90 && corner / DEG_2_RAD < 270)
+		(*x_len) = ((map->p_pos_x +32) % 64) / cos(corner);
+	else
+		(*x_len) = (64 - ((map->p_pos_x +32) % 64)) / cos(corner);
+ 	if (*y_len < 0)
         (*y_len) *= -1;
-	if (*x_len < 0)
-        (*x_len) *= -1;	
+ 	if (*x_len < 0)
+        (*x_len) *= -1;
 }
 
 void	ray_caster(mlx_t *mlx, t_map *map, t_images *images)
 {
-	int				pixels;
-	int				deg;
-	int				ray_size;
-	unsigned int	y;
-	unsigned int	x;
-	bool			pos_wall;
-	bool			neg_wall;
-	int				y_len;
-	int				x_len;
-	bool			neg_deg;
+	int	pixels;
+	int	deg;
+	int	ray_size;
+	int	y_len;
+	int	x_len;
+	int	x;
+	int	y;
 
-	neg_deg = false;
-	y_len = INT_MAX;
-	x_len = INT_MAX;
-	y = 0;
+	pixels = 0;
 	x = 0;
+	y = 0;
 	ray_size = 1024;
-	deg = 0;
+	deg = -33;
 	mlx_delete_image(mlx, images->fg);
 	images->fg = mlx_new_image(mlx, ray_size, ray_size);
 	mlx_image_to_window(mlx, images->fg, map->p_pos_x + 32 - (ray_size / 2), map->p_pos_y + 32 - (ray_size / 2));
  	while (deg < 33)
 	{
-		pixels = 0;
-		pos_wall = false;
-		neg_wall = false;
 		y_len = INT_MAX;
+		x_len = INT_MAX;
 		get_len(map, &x_len, &y_len, deg);
   		while (pixels < (ray_size / 2)  && pixels <= y_len && pixels <= x_len)
 		{
 			mlx_put_pixel(images->fg, (ray_size / 2) + pixels * sin(map->p_orient + deg * DEG_2_RAD), (ray_size / 2) - pixels * cos(map->p_orient + deg * DEG_2_RAD), get_rgba(255, 0, 0, 255));
 			pixels++;
 		}
-		deg++;
+		y = (map->p_pos_y - 32 - pixels * cos(map->p_orient + deg * DEG_2_RAD)) / 64;
+		x = (map->p_pos_x - 32 + pixels * sin(map->p_orient + deg * DEG_2_RAD)) / 64;
+		if (map->map[y][x] == '1' || pixels >= (ray_size / 2))
+		{
+			deg++;
+			//paint the row
+			pixels = 0;
+		}
+		else
+		{
+			mlx_put_pixel(images->fg, (ray_size / 2) + pixels * sin(map->p_orient + deg * DEG_2_RAD), (ray_size / 2) - pixels * cos(map->p_orient + deg * DEG_2_RAD), get_rgba(255, 0, 0, 255));
+			pixels++;
+		}
 	}
-
 }
 
 void	ray_caster2(mlx_t *mlx, t_map *map, t_images *images)
